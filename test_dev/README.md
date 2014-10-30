c3acf35782c29dd32e6ecbc68026e7d4c53236ba
Shawn <niushengqiang@gmail.com>
2014-10-30 17:39:15 +0800

    create backup

---
+++ test_dev/README.md
@@ -0,0 +1,3 @@
+zhengxingji_dev
+===============
+整形记是医疗美容用户交流分享和互动的社区  www.zhengxingji.net
---
+++ test_dev/site/system/config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'aws_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => 'localhost',
+  'username' => 'root',
+  'password' => '',
+  'dbname' => 'zhengxingji',
+);
+$config['slave'] = false;
+
